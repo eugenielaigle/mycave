6024b3292b7680f3e77a796cd6525bd2575b72f6
eugenie <eugenielaigle@gmail.com>
2017-03-11 17:11:20 +0100

    projet

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 try
 {
-$bdd = new PDO('mysql:host=u207501388_mycav;dbname=mycave;charset=utf8', 'u207501388_eugen', '36071503');
+$bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u207501388_mycav;charset=utf8', 'u207501388_eugen', '36071503');
 }
 catch (Exception $e)
 {
