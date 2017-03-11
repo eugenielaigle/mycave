100c55c10d61c164e5cf8e5df52c9de60d40690d
eugenie <eugenielaigle@gmail.com>
2017-03-11 21:56:40 +0100

    modif-responsive

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 try
 {
-$bdd = new PDO('mysql:host=localhost;dbname=mycave;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u207501388_mycav;charset=utf8', 'u207501388_eugen', '36071503');
 }
 catch (Exception $e)
 {
